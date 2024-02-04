04ceb96ae32a76ff20a53836a3abd0f41bc0f5cb
Lawrence Muema Kioko <lorenkioko@gmail.com>
2024-02-04 20:12:18 +0300

    Override Devise send_confirmation_instructions method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,16 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable, :validatable,
-       :recoverable, :rememberable, :lockable, :timeoutable,
+       :recoverable, :rememberable, :timeoutable,
        :jwt_authenticatable, jwt_revocation_strategy: self
+
+       OTP_LENGTH = 6
+
+  def send_confirmation_instructions
+    token = SecureRandom.random_number(10**OTP_LENGTH).to_s.rjust(OTP_LENGTH, "0")
+    self.confirmation_token = token
+    self.confirmation_sent_at = Time.now.utc
+    save(validate: false)
+    UserMailer.confirmation_instructions(self, self.confirmation_token).deliver_now
+  end
 end
